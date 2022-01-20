481c3378229ad8d8c9fc4fb623bac91a03d47b9c
Oneted11 <muriithited@gmail.com>
2022-01-20 19:14:51 +0300

    add localhost tls certs and keys

--- https-server/server.js
+++ https-server/server.js
@@ -1,14 +1,18 @@
 //plain nodejs server, no express used
 //import necessary inbuilt node module
-const http = require("http");
+const https = require("https");
+// import modue to read from filesystem
+const fs = require("fs");
 //create port variable for easier config down the line
 port = 8080;
+// add options object to store tls certificates
+const options = {};
 
 const requestListener = function (req, res) {
   res.writeHead(200);
   res.end("Hello, World!");
 };
 
-const server = http.createServer(requestListener);
+const server = https.createServer(requestListener);
 console.log(`server runing on port ${port}`);
 server.listen(port);
