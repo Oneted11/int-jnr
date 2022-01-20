72e5b62466c9b27c72f175ddecc283756ac40059
Oneted11 <muriithited@gmail.com>
2022-01-20 20:31:37 +0300

    added https and certs, not working

--- https-server/server.js
+++ https-server/server.js
@@ -4,15 +4,18 @@ const https = require("https");
 // import modue to read from filesystem
 const fs = require("fs");
 //create port variable for easier config down the line
-port = 8080;
-// add options object to store tls certificates
-const options = {};
+port = 8081;
+// add options object to store loaded tls certificates
+const options = {
+  key: fs.readFile("./localhost.key", (err) => console.log("error: ", err)),
+  cert: fs.readFile("./localhost.crt", (err) => console.log("error: ", err)),
+};
 
 const requestListener = function (req, res) {
   res.writeHead(200);
   res.end("Hello, World!");
 };
 
-const server = https.createServer(requestListener);
+const server = https.createServer(options, requestListener);
 console.log(`server runing on port ${port}`);
 server.listen(port);
